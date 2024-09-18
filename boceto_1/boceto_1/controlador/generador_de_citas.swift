e46e6d490dbcab633f531f9c53b49f5bc7c4b511
alumno <alumno@lab_quimica.uacj.mx>
2024-09-18 10:36:46 -0600

    [add] controlador

---
+++ boceto_1/boceto_1/controlador/generador_de_citas.swift
@@ -0,0 +1,26 @@
+//
+//  generador_de_citas.swift
+//  boceto_1
+//
+//  Created by alumno on 18-09-24.
+//
+
+import Foundation
+
+class GeneradorDeCitas{
+    var citas_creadas: Array<Cita>
+    init(){
+        citas_creadas = [ ]
+        
+    }
+    
+    func agregar_cita(_ que_dijo: String, quien_lo_dijo:String){
+        var cita_generada = Cita(quien_lo_dijo: quien_lo_dijo, que_dijo: que_dijo)
+        
+        citas_creadas.append(cita_generada)
+    }
+    
+    func imprimir_cita() -> Cita {
+        return citas_creadas[0]
+    }
+}
---
+++ boceto_1/boceto_1/modelo/Cita.swift
@@ -0,0 +1,17 @@
+//
+//  Cita.swift
+//  boceto_1
+//
+//  Created by alumno on 18-09-24.
+//
+
+import Foundation
+
+struct Cita{
+    var nombre: String
+    var texto: String
+    init(quien_lo_dijo: String, que_dijo: String){
+        nombre = quien_lo_dijo
+        texto = que_dijo
+    }
+}
