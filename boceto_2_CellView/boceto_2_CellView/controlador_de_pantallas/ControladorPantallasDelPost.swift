855e13fc4367bf537805bfa654798aaba9def10a
alumno <alumno@lab_quimica.uacj.mx>
2024-10-25 10:35:40 -0600

    [Add] Mostrar nombre de usuario y seccion de comentarios

--- boceto_2_CellView/boceto_2_CellView/controlador_de_pantallas/ControladorPantallasDelPost.swift
+++ boceto_2_CellView/boceto_2_CellView/controlador_de_pantallas/ControladorPantallasDelPost.swift
@@ -8,7 +8,10 @@
 import Foundation
 import UIKit
 
-class ControladorPantallaDelPost: UIViewController {
+class ControladorPantallaDelPost: UIViewController, UICollectionViewDataSource {
+    private let identificador_de_celda = "CeldaComentario"
+   
+    
     let proveedor_publicaciones = ProveedorDePublicaciones.autoreferencia
     
     @IBOutlet weak var titulo_de_publicacion: UILabel!
@@ -26,7 +29,7 @@ class ControladorPantallaDelPost: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("hola mundo")
+        //print("hola mundo")
 
         // Do any additional setup after loading the view.
         
@@ -34,30 +37,43 @@ class ControladorPantallaDelPost: UIViewController {
         let controlador_de_navegacion = self.navigationController as? mod_navegador_principal
         controlador_de_navegacion?.activar_navigation_bar(actviar: true)
         
+        seccion_comentarios.dataSource = self
+        
         realizar_descarga_de_informacion()
     }
     
 
     func realizar_descarga_de_informacion(){
-        if self.publicacion == nil {
-            proveedor_publicaciones.obtener_publicacion(id: self.id_publicacion ?? -1, que_hacer_al_recibir: {
-                [weak self] (publicacion) in self?.publicacion = publicacion
-                DispatchQueue.main.async {
-                    self?.dibujar_publicacion()
-                }
-            })
-        }
-        
-        else if self.publicacion != nil {
-            proveedor_publicaciones.obtener_usuario(id: publicacion!.userId, que_hacer_al_recibir: {
-                [weak self] (usuario) in self?.usuario = usuario
-                DispatchQueue.main.async {
-                    self?.dibujar_publicacion()
-                }
-            })
-        }
-        
-    }
+         if self.publicacion == nil {
+             proveedor_publicaciones.obtener_publicacion(id: self.id_publicacion ?? -1, que_hacer_al_recibir: {
+                 [weak self] (publicacion) in self?.publicacion = publicacion
+                 DispatchQueue.main.async {
+                     self?.dibujar_publicacion()
+                     self?.realizar_descarga_de_informacion()
+                 }
+             })
+         }
+         
+         else if self.publicacion != nil {
+             proveedor_publicaciones.obtener_usuario(id: publicacion!.userId, que_hacer_al_recibir: {
+                 [weak self] (usuario) in self?.usuario = usuario
+                 DispatchQueue.main.async {
+                     self?.dibujar_usuario()
+                 }
+             })
+             
+             proveedor_publicaciones.obtener_comentarios_en_publicacion(id: publicacion!.id, que_hacer_al_recibir: {
+                 [weak self] (comentarios_descargados) in self?.lista_comentarios = comentarios_descargados
+                 DispatchQueue.main.async {
+                     self?.seccion_comentarios.reloadData()
+                 }
+             })
+         }
+         
+         
+         
+     }
+    
     
     func dibujar_publicacion(){
         guard let publicacion_actual = self.publicacion else {
@@ -67,10 +83,33 @@ class ControladorPantallaDelPost: UIViewController {
         titulo_de_publicacion.text = publicacion_actual.title
         cuerpo_de_publicacion.text = publicacion_actual.body
         
-    }
-    
+        }
+    func dibujar_usuario(){
+        guard let usuario_actual = self.usuario else {
+            return
+        }
+        
+       nombre_de_usuario.text = usuario_actual.username
+        
+        }
 
-    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return lista_comentarios.count
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            print("Aqui denberia hacer algo")
+            let celda = collectionView.dequeueReusableCell(withReuseIdentifier: identificador_de_celda, for: indexPath)
+        
+            // Configure the cell
+            celda.tintColor = UIColor.green
+      
+            // print(self.lista_de_publicaciones)
+            
+            return celda
+        }
+        
+
     
     
     
