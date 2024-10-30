6b5e8d85e628019baf7b9b17a8eaf2ff717c0592
alumno <alumno@lab_quimica.uacj.mx>
2024-10-30 09:52:48 -0600

    [ADD] Boceto4 Subir fotos

---
+++ boceto_4_subiendo_fotos/boceto_4_subiendo_fotos/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  boceto_4_subiendo_fotos
+//
+//  Created by alumno on 30-10-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var aqui_va_la_foto: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func subir_foto_para_mostrar(_ sender: Any) {
+        let seleccionar_imagen = UIImagePickerController()
+        seleccionar_imagen.delegate = self
+        
+        seleccionar_imagen.sourceType = UIImagePickerController.SourceType.photoLibrary
+        seleccionar_imagen.allowsEditing = false
+        self.present(seleccionar_imagen, animated: true)
+    }
+        
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let imagen_seleccionada = info[UIImagePickerController.InfoKey.originalImage]as? UIImage{
+            aqui_va_la_foto.image = imagen_seleccionada
+        }
+        else {
+            // Que hacer en caso de ni identificar que selecciono
+        }
+        self.dismiss(animated: true)
+    }
+
+}
+
